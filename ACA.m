2b4210e9470b479a0d556dacb8cde6bd828ea220
ZX19951110 <619186893@qq.com>
2019-01-01 21:36:50 +0100

    2019年01月 1日 周二 21:36:50 增加matlab对加速比的拟合

---
+++ ACA.m
@@ -0,0 +1,7 @@
+cores = [1, 2, 4, 8, 16, 24];
+
+DynamicNohashSpeedup = [0, 1.00, 2.11, 4.25, 8.50, 8.50, 17.00];
+StaticNohashSpeedup = [1.00, 2.11, 3.40, 5.66, 5.66, 5.66];
+
+DynamicHashSpeedup = [0, 1.00, 1.99, 3.92, 7.88, 13.00, 14.89];
+StaticHashSpeedup = [0, 1.00, 2.00, 3.98, 7.54, 12.24, 14.59];
